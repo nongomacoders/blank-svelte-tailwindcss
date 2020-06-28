9c10ed73db56ef5a8a98b6fe4a25c9ded2e51a73
asusdion <dr.dtdavan@gmail.com>
2020-06-28 12:17:42 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1 class="text-center">Helldfo {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+

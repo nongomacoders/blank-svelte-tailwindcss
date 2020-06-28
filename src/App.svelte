87d455283bdbd0c290f52f930ba11b8094a6b925
asusdion <dr.dtdavan@gmail.com>
2020-06-28 14:40:40 +0200

    modal and store

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,16 @@
 <script>
-	export let name;
+  import Modal from "./Modal.svelte";
+  import {showModal} from "./stores.js";
 </script>
 
-<main>
-	<h1 class="text-center">Helldfo {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<p>
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum unde
+  consequuntur, cum fugit deleniti asperiores, reiciendis impedit est rerum
+  quisquam quo eaque placeat, facilis sit laborum neque dolorem officia.
+  Nostrum.
+</p>
+{#if $showModal}
+	<Modal>hello</Modal>
+{/if}
+<button on:click={()=>showModal.set(1)}>Show modal</button>
 
---
+++ src/Modal.svelte
@@ -0,0 +1,60 @@
+<script>
+import Button from './Button.svelte'
+ import {showModal} from "./stores.js";
+function closeModal(){
+showModal.set(0);
+}
+</script>
+
+<style>
+
+
+  .modal {
+    position: fixed;
+    top: 0;
+    left:0;    
+    width: 100%;
+    max-height: 80vh;
+    background: white;
+    
+    z-index: 100;
+ 
+    overflow: scroll;
+  }
+
+  h1 {
+    padding: 1rem;
+    margin: 0;
+    border-bottom: 1px solid #ccc;
+    font-family: "Roboto Slab", sans-serif;
+  }
+
+  .content {
+    padding: 1rem;
+  }
+
+  footer {
+    padding: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    .modal {
+      width: 40rem;
+      left: calc(50% - 20rem);
+    }
+  }
+</style>
+
+
+
+<div class="modal">
+  <span>Edit</span><Button on:click={closeModal}>Close</Button>
+  <div class="content">
+    <slot />
+  </div>
+  <footer>
+    <slot name="footer">
+      <Button on:click={closeModal}>Close</Button>
+    </slot>
+  </footer>
+</div>
